d1aa7326ce5ea7f6aeff7152fc7721645f584414
Wender <wenderliborio@gmail.com>
2019-01-21 12:21:04 -0400

    initial setup

---
+++ Assets/Magnifier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Magnifier : MonoBehaviour {
+	public Camera myCamera;
+	public Image magnifier;
+	public Vector3 offset;
+	void Update(){
+		if(Input.GetMouseButton(0)){
+			magnifier.transform.position = Input.mousePosition ;
+		}
+		myCamera.transform.localPosition = magnifier.transform.localPosition + offset;
+		
+	}
+	
+	
+
+
+}
